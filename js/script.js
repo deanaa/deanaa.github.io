27722c07cb6b66992365588db583ff01b9dbcd0a
deanaa <kentangcreatspy@gmail.com>
2021-04-16 15:13:31 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(".page-scroll").click(function (e) {
+
+    var tujuan = $(this).attr('href');
+
+    var elemenTujuan = $(tujuan);
+
+    $('html, body').animate({
+        scrollTop: $(elemenTujuan).offset().top - 50
+    }, 2000, 'easeInOutExpo');
+
+    e.preventDefault();
+});
+
+$(window).on('load', function () {
+    $('.pLeft').addClass('pShow');
+    $('.pRight').addClass('pShow');
+});
+
+$(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+
+    $('.jumbotron img').css({
+        'transform': 'translate(0px, ' + wScroll / 4 + '%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform': 'translate(0px, ' + wScroll / 2 + '%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform': 'translate(0px, ' + wScroll / 1.2 + '%)'
+    });
+
+    if (wScroll > $('.portfolio').offset().top - 250) {
+        $('.portfolio .thumbnail').each(function (i) {
+            setTimeout(function () {
+                $('.portfolio .thumbnail').eq(i).addClass('show');
+            }, 300 * (i + 1));
+        });
+    }
+
+});
